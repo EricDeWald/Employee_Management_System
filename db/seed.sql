15bf1dd835b6b0388419abc9e6c35e4c45022b78
EricDeWald <eric.l.dewald@gmail.com>
2022-01-25 22:07:43 -0600

    Updated seed.sql
    added some formatting and corrections to the seed data so they would show correctly in the terminal.

--- db/seed.sql
+++ db/seed.sql
@@ -1,12 +1,12 @@
-INSERT INTO department (id, name)
-VALUES (1,"The Ring"),(2,"The Box");
+INSERT INTO department (name)
+VALUES ("The Ring"),("The Box");
 
-INSERT INTO role (id, title, salary)
-VALUES (1, "pres", "10"),(2, "VP", "20")
+INSERT INTO role (title, salary)
+VALUES ("pres", "10"),("VP", "20");
 
-INSERT INTO employee (id,first_name,last_name)
-VALUES (1,"King","Koopa");(2,"Koopa","Troopa")
+INSERT INTO employee (first_name,last_name)
+VALUES ("King","Koopa"),("Koopa","Troopa");
 
-SELECT * FROM department
-SELECT * FROM role
-SELECT * FROM employee
+SELECT * FROM department;
+SELECT * FROM role;
+SELECT * FROM employee;
