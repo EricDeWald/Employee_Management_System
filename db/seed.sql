5247595925dbeb3b0ab87dd432799569a3418e66
EricDeWald <eric.l.dewald@gmail.com>
2022-01-25 21:49:53 -0600

    updates to schema and seed
    updated connections for schema keys and added seed table data

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+INSERT INTO department (id, name)
+VALUES (1,"The Ring"),(2,"The Box");
+
+INSERT INTO role (id, title, salary)
+VALUES (1, "pres", "10"),(2, "VP", "20")
+
+INSERT INTO employee (id,first_name,last_name)
+VALUES (1,"King","Koopa");(2,"Koopa","Troopa")
+
+SELECT * FROM department
+SELECT * FROM role
+SELECT * FROM employee
