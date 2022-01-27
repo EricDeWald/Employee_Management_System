e587d7c09682d0e478d99a6c97f476d864e063cc
EricDeWald <eric.l.dewald@gmail.com>
2022-01-27 14:37:41 -0600

    Setting up some files
    set up some files for modularity. Added more values to my test table data.

--- db/seed.sql
+++ db/seed.sql
@@ -1,11 +1,11 @@
 INSERT INTO department (name)
-VALUES ("The Ring"),("The Box");
+VALUES ("The Ring"),("The Box"),("The Ship");
 
 INSERT INTO role (title, salary)
-VALUES ("pres", "10"),("VP", "20");
+VALUES ("pres", "100"),("VP", "50"),("Goon","1"),('Tug','3');
 
 INSERT INTO employee (first_name,last_name)
-VALUES ("King","Koopa"),("Koopa","Troopa");
+VALUES ("King","Koopa"),("Koopa","Troopa"),("Goomba","Mushroom")("Green","Tutle");
 
 SELECT * FROM department;
 SELECT * FROM role;
---
+++ init.js
@@ -0,0 +1,7 @@
+//make sure you require
+
+
+// us this to call all your functions to un program
+
+
+init = function(){}
